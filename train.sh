f7f9934e6b18f21cd1e8314fc9a8820a3b129fa2
Duchstf <minhduc8199@gmail.com>
2019-09-21 21:30:53 -0500

    add training batch jobs

---
+++ train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=RF-train
+#SBATCH --output=RF-train.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=5000
+
+conda activate mnist-training
+cd ~/RF/hls4ml-RF/
+python3 RF-prune.py
+
+
+
